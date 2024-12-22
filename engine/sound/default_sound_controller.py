f7b1633dc4e183f455becc2c85badc02b4f80a6f
Aaron Wilson <acwilsoncs@gmail.com>
2024-12-21 22:12:22 -0700

    fix: bypass audio error

--- engine/sound/default_sound_controller.py
+++ engine/sound/default_sound_controller.py
@@ -1,6 +1,7 @@
 import logging
 
 from pygame import mixer
+import pygame
 
 import settings
 from engine.sound.sound_controller import SoundController
@@ -8,10 +9,17 @@ from engine.sound.sound_controller import SoundController
 
 class DefaultSoundController(SoundController):
     def __init__(self):
-        # https://stackoverflow.com/a/20021547
-        mixer.init()
+        try:
+            mixer.init()
+        except pygame.error:
+            print("Warning: Audio device not available, sound will be disabled.")
+            self.sound_enabled = False
+        else:
+            self.sound_enabled = True
 
     def play(self, track: str):
+        if not self.sound_enabled:
+            return
         if track not in tracks:
             logging.warning(f"DefaultSoundController: missing sound track {track}")
             return
@@ -23,7 +31,6 @@ class DefaultSoundController(SoundController):
 
         mixer.music.load(resource)
         mixer.music.play(fade_ms=2000)
-
 
 tracks = {
     "theme": "./resources/theme.ogg",
