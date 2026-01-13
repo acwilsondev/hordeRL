bbacd0337f1407069df82df2fc407928a1f199e4
Aaron Wilson <1923150+acwilsondev@users.noreply.github.com>
2026-01-13 11:25:31 -0700

    Fix help dialogue progression (#41)
    * Fix help dialogue progression
    
    * Add help dialogue test
    
    * .

--- horderl/components/events/show_help_dialogue.py
+++ horderl/components/events/show_help_dialogue.py
@@ -1,12 +1,19 @@
 from engine.components import EnergyActor
+from horderl.gui.help_dialogue import HelpDialogue
 from horderl.i18n import t
 
 
 class ShowHelpDialogue(EnergyActor):
     def act(self, scene) -> None:
-        scene.popup_message(t("help.dialogue.intro"))
-        scene.popup_message(t("help.dialogue.controls"))
-        scene.popup_message(t("help.dialogue.money"))
-        scene.popup_message(t("help.dialogue.money_pt2"))
-        scene.popup_message(t("help.dialogue.attacks"))
+        messages = [
+            t("help.dialogue.intro"),
+            t("help.dialogue.controls"),
+            t("help.dialogue.money"),
+            t("help.dialogue.money_pt2"),
+            t("help.dialogue.attacks"),
+        ]
+        if hasattr(scene, "message"):
+            for message in messages:
+                scene.message(message)
+        scene.add_gui_element(HelpDialogue(messages, scene.config))
         scene.cm.delete_component(self)
